191aaf135f09de0f4193080adc46a9d22491190e
Rodrigo Gonzalez <rdgonzalez85@gmail.com>
2025-08-08 02:16:54 +0100

    extracted constants into a separated struct, reorganised code

--- Recipes/Details/View/InfoItem.swift
+++ Recipes/Details/View/InfoItem.swift
@@ -3,15 +3,39 @@ import SwiftUI
 struct InfoItem: View {
     let title: String
     let value: String
+    private let constants: InfoItemConstants
     
+    init(
+        title: String,
+        value: String,
+        constants: InfoItemConstants = InfoItemConstants()
+    ) {
+        self.title = title
+        self.value = value
+        self.constants = constants
+    }
     var body: some View {
         VStack(spacing: 2) {
             Text(value)
-                .font(.subheadline)
+                .font(self.constants.text.valueFont)
                 .bold()
             Text(title)
-                .font(.caption)
-                .foregroundColor(.secondary)
+                .font(self.constants.text.titleFont)
+                .foregroundColor(self.constants.colors.title)
         }
     }
+}
+
+struct InfoItemConstants {
+    struct Text {
+        let valueFont: Font = .subheadline
+        let titleFont: Font = .caption
+    }
+    
+    struct Colors {
+        let title: Color = .secondary
+    }
+
+    let text = Text()
+    let colors = Colors()
 }
--- Recipes/Home/View/RecipeRowView.swift
+++ Recipes/Home/View/RecipeRowView.swift
@@ -12,56 +12,63 @@ struct RecipeRowView: View {
     
     var body: some View {
         HStack {
-            // it may be worth using a 3rd party library instead of asyncImage, such as https://github.com/kean/Nuke
-            AsyncImage(url: URL(string: recipe.image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: self.constants.image.aspectRatio)
-            } placeholder: {
-                Rectangle()
-                    .foregroundColor(self.constants.image.placeholderColor)
-            }
-            .frame(width: self.constants.image.size, height: self.constants.image.size)
-            .clipShape(RoundedRectangle(cornerRadius: self.constants.image.cornerRadius))
-            
+            recipeImage
             VStack(alignment: .leading, spacing: self.constants.layout.spacing) {
-                Text(recipe.name)
-                    .font(self.constants.text.nameFont)
-                    .lineLimit(self.constants.text.recipeLineLimit)
-                
+                recipeNameView
                 HStack {
-                    Image(systemName: self.constants.icon.starFill)
-                        .foregroundColor(self.constants.icon.foregroundColor)
-                        .font(self.constants.text.starRatingFont)
-                    
-                    Text(recipe.rating)
-                        .font(self.constants.text.starRatingFont)
-                        .foregroundColor(self.constants.rating.foregroundColor)
-                    
+                    starIcon
+                    ratingView
                     Spacer()
-                    
-                    Text(recipe.difficulty)
-                        .font(self.constants.text.difficultyFont)
-                        .padding(.horizontal, self.constants.layout.horizontalPadding)
-                        .padding(.vertical, self.constants.layout.verticalDifficultyPadding)
-                        .background(difficultyColor.opacity(self.constants.layout.difficultyOpacity))
-                        .foregroundColor(difficultyColor)
-                        .clipShape(Capsule())
+                    recipeDifficultyView
                 }
             }
-            
             Spacer()
         }
         .padding(.vertical, self.constants.layout.verticalPadding)
     }
     
-    private var difficultyColor: Color {
-        switch recipe.difficulty.lowercased() {
-        case "easy": return .green
-        case "medium": return .orange
-        case "hard": return .red
-        default: return .gray
+    private var recipeImage: some View {
+        // it may be worth using a 3rd party library instead of asyncImage, such as https://github.com/kean/Nuke
+        AsyncImage(url: URL(string: recipe.image)) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: self.constants.image.aspectRatio)
+        } placeholder: {
+            Rectangle()
+                .foregroundColor(self.constants.image.placeholderColor)
         }
+        .frame(width: self.constants.image.size, height: self.constants.image.size)
+        .clipShape(RoundedRectangle(cornerRadius: self.constants.image.cornerRadius))
+    }
+    
+    private var recipeNameView: some View {
+        Text(recipe.name)
+            .font(self.constants.text.nameFont)
+            .lineLimit(self.constants.text.recipeLineLimit)
+    }
+    
+    private var starIcon: some View {
+        Image(systemName: self.constants.icon.starFill)
+            .foregroundColor(self.constants.icon.foregroundColor)
+            .font(self.constants.text.starRatingFont)
+    }
+    
+    private var ratingView: some View {
+        Text(recipe.ratingString)
+            .font(self.constants.text.starRatingFont)
+            .foregroundColor(self.constants.rating.foregroundColor)
+    }
+    
+    @ViewBuilder
+    private var recipeDifficultyView: some View {
+        let difficultyColor = recipe.difficulty.color(colors: self.constants.colors)
+        Text(recipe.difficulty.rawValue)
+            .font(self.constants.text.difficultyFont)
+            .padding(.horizontal, self.constants.layout.horizontalPadding)
+            .padding(.vertical, self.constants.layout.verticalDifficultyPadding)
+            .background(difficultyColor.opacity(self.constants.colors.difficultyOpacity))
+            .foregroundColor(difficultyColor)
+            .clipShape(Capsule())
     }
 }
 
@@ -88,7 +95,6 @@ struct RecipeRowConstants {
         let verticalPadding: CGFloat = 4
         let horizontalPadding: CGFloat = 8
         let verticalDifficultyPadding: CGFloat = 2
-        let difficultyOpacity: Double = 0.2
     }
     
     struct Text {
@@ -98,6 +104,15 @@ struct RecipeRowConstants {
         let nameFont: Font = .headline
     }
     
+    struct Colors: DifficultyColors {
+        let difficultyEasy: Color = .green
+        let difficultyMedium: Color = .orange
+        let difficultyHard: Color = .red
+        let difficultyDefault: Color = .gray
+        let difficultyOpacity: Double = 0.2
+    }
+    
+    let colors = Colors()
     let image = Image()
     let icon = Icon()
     let layout = Layout()
